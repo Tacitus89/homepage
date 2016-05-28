949606ac5603b361882df58d1b21b8542cda310e
Tacitus89 <marco.can@gmx.de>
2016-05-28 16:24:09 +0200

    Fixed small install bug

--- migrations/install_0_0_1.php
+++ migrations/install_0_0_1.php
@@ -48,7 +48,7 @@ class install_0_0_1 extends \phpbb\db\migration\migration
 					'hp_post_id'		=> array('UINT:8', 0),
 					'hp_meta'			=> array('VCHAR:255', ''),
 					'hp_desc'			=> array('VCHAR:255', ''),
-					'hp_gallery'		=> array('UINT:8', 0),
+					'hp_gallery_id'		=> array('UINT:8', 0),
 					'hp_game_id'		=> array('UINT:8', 0),
 				),
 			),
@@ -65,7 +65,7 @@ class install_0_0_1 extends \phpbb\db\migration\migration
 	{
 		return array(
 			'drop_columns'	=> array(
-				$this->table_prefix . 'forums' => array('hp_show', 'hp_special', 'hp_name', 'hp_post_id','hp_meta', 'hp_desc', 'hp_gellery_id', 'hp_game_id'),
+				$this->table_prefix . 'forums' => array('hp_show', 'hp_special', 'hp_url', 'hp_post_id','hp_meta', 'hp_desc', 'hp_gallery_id', 'hp_game_id'),
 			),
 		);
 	}
